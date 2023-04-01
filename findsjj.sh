1cce0699005a1688b430efafed29e30e96b0c142
vangquan <vangquan@vangquan.com>
2023-04-01 18:06:44 +0700

    Add files via upload

---
+++ findsjj.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ask for song number
+read -p "Write the song number you want to find: " num_input
+
+# Make sure num has 3 digits
+num=$(printf "%03d" "$num_input")
+
+# Get all the current signlanguage mnamonic on JW.ORG
+sjjs=$(curl -s 'https://data.jw-api.org/mediator/v1/languages/E/all' | jq -r '.languages | map(select(.isSignLanguage == true)) | map(.code) | .[]')
+
+# Check each sign language for song num_input for download links
+for sjj_lang in ${sjjs[@]}; do
+    curl -s "https://data.jw-api.org/mediator/v1/categories/${sjj_lang}/VODSJJMeetings" | jq | grep -Eo "https:\/\/[a-zA-Z0-9./?=_%:-]*${num}_r720P\.mp4"
+done
