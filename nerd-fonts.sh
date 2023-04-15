23e700939aa9d27d84bd39e422ecb78175bdf28a
vangquan <vangquan@vangquan.com>
2023-04-15 09:43:15 +0700

    Add files via upload

--- nerd-fonts.sh
+++ nerd-fonts.sh
@@ -1,73 +1,63 @@
 #!/bin/bash
+# Nerd-font installation script
 
-declare -a fonts=(
-	Agave
-	AnonymousPro
-	Arimo
-	AurulentSansMono
-	BigBlueTerminal
-	BitstreamVeraSansMono
-	CascaidaCode
-	CodeNewRoman
-	Cousine
-	DaddyTimeMono
-	DejaVuSansMono
-	DroidSansMono
-	FantasqueSansMono
-	FiraCode
-	FiraMono
-	Go-Mono
-	Gohu
-	Hack
-	Hasklig
-	HeavyData
-	Hermit
-	iA-Writer
-	IBMPlexMono
-	Inconsolate
-	InconsolataGo
-	InconsolataLGC
-	Iosevka
-	JetBrainsMono
-	Lekton
-	LiberationMono
-	Lilex
-	Meslo
-	Monofur
-	Mononoki
-	Monoid
-	MPlus
-	NerdFontsSymbolsOnly
-	Noto
-	OpenDyslexic
-	Overpass
-	ProFont
-	ProggyClean
-	RobotoMono
-	ShareTechMono
-	Terminus
-	Tinos
-	Ubuntu
-	UbuntuMono
-	VictorMono
-)
+show_fonts() {
+  echo "1. FiraCode"
+  echo "2. Hack"
+  echo "3. SourceCodePro"
+  echo "4. JetBrainsMono"
+}
 
-version='2.2.2'
-fonts_dir="${HOME}/.local/share/fonts"
+get_font_choice() {
+  echo "Select a font:"
+  show_fonts
+  read -p "Enter the font number: " font_choice
+  case $font_choice in
+    1) font="FiraCode" ;;
+    2) font="Hack" ;;
+    3) font="SourceCodePro" ;;
+    4) font="JetBrainsMono" ;;
+    *) echo "Invalid font choice. Please try again."; get_font_choice ;;
+  esac
+}
 
-if [[ ! -d "$fonts_dir" ]]; then
-    mkdir -p "$fonts_dir"
-fi
+get_os_choice() {
+  echo "Select your OS:"
+  echo "1. Ubuntu"
+  echo "2. Fedora"
+  echo "3. Arch"
+  echo "4. macOS"
+  read -p "Enter the OS number: " os_choice
+  case $os_choice in
+    1) os="Ubuntu" ;;
+    2) os="Fedora" ;;
+    3) os="Arch" ;;
+    4) os="macOS" ;;
+    *) echo "Invalid OS choice. Please try again."; get_os_choice ;;
+  esac
+}
 
-for font in "${fonts[@]}"; do
-    zip_file="${font}.zip"
-    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
-    echo "Downloading $download_url"
-    wget "$download_url"
-    unzip "$zip_file" -d "$fonts_dir"
-    rm "$zip_file"
-done
+install_font() {
+  echo "Installing $font for $os..."
+  font_url="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/$font/Regular/complete/${font// /}%20Nerd%20Font%20Complete.ttf"
+  temp_font_path="/tmp/${font// /}_Nerd_Font_Complete.ttf"
+  wget -q --show-progress -O "$temp_font_path" "$font_url"
 
-find "$fonts_dir" -name '*Windows Compatible*' -delete
+  case $os in
+    "Ubuntu" | "Fedora" | "Arch")
+      install_dir="$HOME/.local/share/fonts"
+      mkdir -p "$install_dir"
+      cp "$temp_font_path" "$install_dir/"
+      fc-cache -f -v
+      ;;
+    "macOS")
+      install_dir="$HOME/Library/Fonts"
+      cp "$temp_font_path" "$install_dir/"
+      ;;
+  esac
+  echo "Successfully installed $font for $os."
+}
 
-fc-cache -fv
+get_font_choice
+get_os_choice
+install_font
