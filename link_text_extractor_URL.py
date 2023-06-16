91897d9dfac4edb496df25809285aaf307e6c012
vangquan <vangquan@vangquan.com>
2023-06-17 02:28:46 +0700

    Add files via upload

---
+++ link_text_extractor_URL.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+import webbrowser
+import urllib.parse
+
+# check if URL is provided as an argument
+if len(sys.argv) < 2:
+    print('Usage: python script.py <URL>')
+    sys.exit(1)
+
+# get the URL from command-line arguments
+url = sys.argv[1]
+
+# send a HTTP request to the specified URL
+r = requests.get(url)
+
+# create a BeautifulSoup object and specify the parser
+soup = BeautifulSoup(r.text, 'html.parser')
+
+# find the article with the specified id
+article = soup.find('article', id='article')
+
+if article is not None:
+    # find all 'em' tags within the article
+    em_tags = article.find_all('em')
+    
+    # remove all 'em' tags but keep their contents
+    for tag in em_tags:
+        tag.unwrap()
+
+    # find all hyperlink tags within the article
+    links = article.find_all('a')
+    
+    # get their full text, including the text within any nested tags
+    links_text = [a.get_text() for a in links if a.get_text() is not None and any(char.isdigit() for char in a.get_text())]
+    
+    # join all the link text into a single string separated by semicolons
+    result = ';'.join(links_text)
+
+    # print results
+    # print(result)
+
+    # URL encode the result
+    result = urllib.parse.quote(result)
+
+    # format the JW search URL with the result
+    search_url = f'https://wol.jw.org/vi/wol/l/r47/lp-vt?q={result}'
+
+    # open the search URL in the default web browser
+    webbrowser.open(search_url)
+else:
+    print("The specified article id was not found.")
